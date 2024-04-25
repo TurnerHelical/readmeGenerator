8be9c7857dc6bd07cc880139c41ad11ce3b312aa
Hunter <hunterde91@gmail.com>
2024-04-24 20:12:48 -0400

    added the prompts for the generator

--- index.js
+++ index.js
@@ -9,9 +9,34 @@ const questions =[
         },
         {
             type: 'input',
+            message: 'What is the description for your project?',
+            name: 'description',
+        },
+        {
+            type: 'input',
             message: 'What is the title of your project?',
             name: 'title',
-        }
+        },
+        {
+            type: 'input',
+            message: 'What are the installation instructions for your project?',
+            name: 'installInstructions',
+        },
+        {
+            type: 'input',
+            message: 'What would the user do with this project? Examples?',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'How can other contribute to your project?',
+            name: 'contribution',
+        },
+        {
+            type: 'input',
+            message: 'How can the user test your project?',
+            name: 'test',
+        },
 ]
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
